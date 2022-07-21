29995e9ecaae563f219b1f58b127e2d61861fdcb
jisu-d <lim16111611@gmail.com>
2022-07-21 15:10:39 +0900

    cook err 해결

--- main/src/server.ts
+++ main/src/server.ts
@@ -190,7 +190,13 @@ export const fetchCookInfo = async (schoolName:string, getNum:number) => { //급
     
     
     const res = await (await fetch(`${neisApis.급식식단정보}?KEY=${neisApis.key}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${arr.ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${arr.SD_SCHUL_CODE}&MLSV_FROM_YMD=${dayList[0]}&MLSV_TO_YMD=${dayList[1]}`)).json()
-   
+    if(res){
+        return res
+    } else{
+        return ({
+            msg:'데이터가 없습니다.'
+        })
+    }
     
     return res
 }
