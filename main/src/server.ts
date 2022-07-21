c7c75debc9830171b0a2b3d7623902efd387d8b6
jisu-d <lim16111611@gmail.com>
2022-07-21 15:19:23 +0900

    err

--- main/src/server.ts
+++ main/src/server.ts
@@ -188,27 +188,22 @@ export const fetchCookInfo = async (schoolName:string, getNum:number) => { //급
         }
     }
 
-    console.log(`${neisApis.급식식단정보}?KEY=${neisApis.key}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${arr.ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${arr.SD_SCHUL_CODE}&MLSV_FROM_YMD=${dayList[0]}&MLSV_TO_YMD=${dayList[1]}`);
-    
-    
-    
-    const res = await (await fetch(`${neisApis.급식식단정보}?KEY=${neisApis.key}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${arr.ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${arr.SD_SCHUL_CODE}&MLSV_FROM_YMD=${dayList[0]}&MLSV_TO_YMD=${dayList[1]}`)).json()
-    if(res){
-        return res
-    } else{
-        return ({
-            msg:'데이터가 없습니다.'
-        })
-    }
+    const res = await (await fetch(`${neisApis.급식식단정보}?KEY=${neisApis.key}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${arr.ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${arr.SD_SCHUL_CODE}&MLSV_FROM_YMD=${dayList[0]}&MLSV_TO_YMD=${dayList[1]}`)).json();
     
     return res
 }
 
 export const checkSchool = async (schoolName:string, year:number, Class:number) => {
     const schoolInfo = await fetchSchoolInfo(schoolName)
+
+    console.log(schoolInfo);
+    
     const date = new Date()
     
     const res: msg | sc = await (await fetch(`${neisApis.반정보}?KEY=${neisApis.key}&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=${schoolInfo.ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${schoolInfo.SD_SCHUL_CODE}&AY=${date.getFullYear()}&GRADE=${year}`)).json()
+
+    console.log(res);
+    
     
     if ('classInfo' in res) {
         if(res.classInfo[1].row.length >= Class){
