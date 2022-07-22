09ef71449e175a2febd0c3e030bf91b13a92df62
jisu-d <lim16111611@gmail.com>
2022-07-22 16:12:06 +0900

    cook err 해결

--- public/type.d.ts
+++ public/type.d.ts
@@ -31,19 +31,21 @@ export type mSDI= {
             ]
         },
         {
-            row:{
+            row: {
                 ATPT_OFCDC_SC_CODE: string
                 ATPT_OFCDC_SC_NM: string
-                AY: string
-                CLASS_NM: string
-                DDDEP_NM: string
-                DGHT_CRSE_SC_NM: string
-                GRADE: string
-                LOAD_DTM: string
-                ORD_SC_NM: string
-                SCHUL_CRSE_SC_NM: string
+                CAL_INFO: string
+                DDISH_NM: string
+                MLSV_FGR: string
+                MLSV_FROM_YMD: string
+                MLSV_TO_YMD: string
+                MLSV_YMD: string
+                MMEAL_SC_CODE: string
+                MMEAL_SC_NM: string
+                NTR_INFO: string
+                ORPLC_INFO: string
                 SCHUL_NM: string
-                SD_SCHUL_CODE: string  
+                SD_SCHUL_CODE: string
             }[]
         }
     ]
