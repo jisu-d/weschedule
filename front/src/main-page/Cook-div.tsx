974557847eb9b00274fe5eaf68b5e97033da12d9
jisu-d <lim16111611@gmail.com>
2022-07-22 13:48:52 +0900

    cook err 해결

--- front/src/main-page/Cook-div.tsx
+++ front/src/main-page/Cook-div.tsx
@@ -18,33 +18,23 @@ export function CookDiv() {
 
 const createCook = () => {
     const arr:JSX.Element[] = [];
-    console.log(da);
-    
-    if(da === '해당하는 데이터가 없습니다.'){
-        console.log(1);
-        return(
-            <div>{da}</div>
-        )
-    } else{
-        for(let i = 0 ; i < da.length; i++){
-            const element = (
-                <div className="cooktable" key={`cook-${i}`}>
-                    <div className='kcal'>
-                        <span>{da[i].CAL_INFO}</span>
-                    </div>
-                    <div className='food-list'>{da[i].DDISH_NM.replace(/[\.0-9\(\)]/g, '').split('<br/>').map((v:string, j:number) => (<div key={`cook-menu-${j}`}>{v}</div>))}</div>
-                                                           
-                    <div className='date'>{da[i].MLSV_TO_YMD.slice(4).replace(/(\d{2})(\d{2})/, '$1월 $2일')}</div>
+    for(let i = 0 ; i < da.length; i++){
+        const element = (
+            <div className="cooktable" key={`cook-${i}`}>
+                <div className='kcal'>
+                    <span>{da[i].CAL_INFO}</span>
                 </div>
-            )
-            arr.push(element)
-        }
-        console.log(2);
-        
-        return (
-            <>
-                {arr}
-            </>
+                <div className='food-list'>{da[i].DDISH_NM.replace(/[\.0-9\(\)]/g, '').split('<br/>').map((v:string, j:number) => (<div key={`cook-menu-${j}`}>{v}</div>))}</div>
+                                                       
+                <div className='date'>{da[i].MLSV_TO_YMD.slice(4).replace(/(\d{2})(\d{2})/, '$1월 $2일')}</div>
+            </div>
         )
+        arr.push(element)
     }
+    
+    return (
+        <>
+            {arr}
+        </>
+    )
 }
