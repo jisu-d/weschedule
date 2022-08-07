583202ded598fbaacfb40b4ac106944e440710ba
jisu-d <lim16111611@gmail.com>
2022-08-07 14:39:07 +0900

    Sky fetch add

--- main/dist/server.js
+++ main/dist/server.js
@@ -201,6 +201,7 @@ export const getSkyData = async (lat, lng) => {
     else {
         baseTime = `${Math.floor(((hour - 1) - 2) / 3) * 3 + 2}`.padStart(2, '0') + '00';
     }
+    console.log(baseTime);
     console.log(`${SkyUrl.url}?serviceKey=${SkyUrl.key}&pageNo=1&numOfRows=14&dataType=JSON&base_date=${base_date}&base_time=${baseTime}&nx=${xydata.x}&ny=${xydata.y}`);
     const fetchData = await (await fetch(`${SkyUrl.url}?serviceKey=${SkyUrl.key}&pageNo=1&numOfRows=14&dataType=JSON&base_date=${base_date}&base_time=${baseTime}&nx=${xydata.x}&ny=${xydata.y}`)).json();
     const reData = [];
