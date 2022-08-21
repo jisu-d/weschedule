48fe88226a85b5eef0b3b986515088a2982f27f1
jisu-d <lim16111611@gmail.com>
2022-08-21 14:12:13 +0900

    cors 수정

--- main/src/index.ts
+++ main/src/index.ts
@@ -7,7 +7,8 @@ const fastify = Fastify({
     logger:true
 });
 fastify.register(fastifyCors, {
-    origin:"https://jisu-d.github.io"
+    // origin:"https://jisu-d.github.io"
+    origin:"*"
 });
 
 fastify.register(route, {prefix:'/'});//이거 슈밤바/f/main 해야함 /* 모든것
