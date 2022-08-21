d406bf6d5a39af26a4cd5cc73130436bdd51e4ea
jisu-d <lim16111611@gmail.com>
2022-08-21 14:24:10 +0900

    proxy 변경

--- front/src/main-page/Sky_div.tsx
+++ front/src/main-page/Sky_div.tsx
@@ -2,27 +2,7 @@ import './Sky_div'
 import { sever_Sky, Skydata } from '../../../public/type';
 
 import { proxy } from "../proxy";
-
-
-// navigator.geolocation.getCurrentPosition(async (pos) => {
-//     const la = pos.coords.latitude
-//     const lo = pos.coords.longitude
-//     if(la !== 0 && lo !== 0){
-//         da = await(await fetch(`${proxy}/Skydata?x=${la}&y=${lo}`)).json()
-//     }
-// })
-
-// export function Sky_div() {
-//     const arr:JSX.Element[] = []
-//     da.data.map((v:string) => arr.push(<div>{v}</div>))
-//     return(
-//         <div>
-//             <div>
-//                 {arr}
-//             </div>
-//         </div>
-//     )
-// }
+import React, { useEffect } from "react";
 
 let la = 0
 let lo = 0
@@ -32,26 +12,47 @@ navigator.geolocation.getCurrentPosition((pos) => {
     lo = pos.coords.longitude
 })
 
-const SkyDiv = async () => {
-    const apiKey = 'd931b3df313d8586e334f45873e59273'
-    const da: Skydata = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apiKey}&lang=kr`)).json()
-    const imgUrl = `http://openweathermap.org/img/wn/${da.weather[0].icon}@2x.png`
+const apiKey = 'd931b3df313d8586e334f45873e59273'
+const da: Skydata = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apiKey}&lang=kr`)).json()
 
-    return(
-        <div>
-            <div>
-                <img src={imgUrl} />
-                <div>{da.weather[0].description}°C</div>
-            </div>
-            <div>현재온도 {da.main.feels_like}도</div>
-            <div>{da.weather[0].description}</div>
-        </div>
-    )
-}
-
+let skyImg: string
+let temp: number
+let description: string
+let feelsLike: number
+// useEffect(() => {
+//     skyImg = `http://openweathermap.org/img/wn/${da.weather[0].icon}@2x.png`
+//     temp = da.main.temp
+//     description = da.weather[0].description
+//     feelsLike = da.main.feels_like
+// }, [da])
 
 export function Sky_div() {
+
+    const element = (
+        <div>
+            <div>
+                <img src={`http://openweathermap.org/img/wn/${da.weather[0].icon}@2x.png`} />
+                <div>{da.main.temp}°C</div>
+            </div>
+            <div>현재온도 {da.weather[0].description}도</div>
+            <div>{da.main.feels_like}</div>
+        </div>
+    )
+
+    useEffect(() => {
+        return () => {
+            element
+        }
+    }, [element])
+
     return (
-        SkyDiv()
+        <div>
+            <div>
+                <img src={skyImg} />
+                <div>{temp}°C</div>
+            </div>
+            <div>현재온도 {description}도</div>
+            <div>{feelsLike}</div>
+        </div>
     )
 }
