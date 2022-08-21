e22c25777f3797d3c8c882f0bb08d70a01a1a297
jisu-d <lim16111611@gmail.com>
2022-08-21 22:32:41 +0900

    cors origin 수정

--- main/dist/index.js
+++ main/dist/index.js
@@ -7,6 +7,7 @@ const fastify = Fastify({
 });
 fastify.register(fastifyCors, {
     origin: "https://jisu-d.github.io"
+    // origin:"/*"
 });
 fastify.register(route, { prefix: '/' }); //이거 슈밤바/f/main 해야함 /* 모든것
 fastify.get('/schoolList', async (req, rep) => {
