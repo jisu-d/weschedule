30b6567dd1f4ccefcdeb87ad35ad8862e7ae573d
jisu-d <lim16111611@gmail.com>
2022-08-23 00:07:38 +0900

    err 수정

--- front/src/main-page/Sky_div.tsx
+++ front/src/main-page/Sky_div.tsx
@@ -17,12 +17,27 @@ import React, { useEffect, useState } from "react";
 export function Sky_div() {
     let la = 0
     let lo = 0
-    let [da, setDa] = useState<Skydata>(null);
     let [arr, setArr] = useState<JSX.Element>();
     const getData = async () => {
         const apiKey = 'd931b3df313d8586e334f45873e59273'
         const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apiKey}&units=metric&lang=kr`)).json();
-        setDa(data);
+        setArr(
+            <>
+                <div className='img_temp_container'>
+                    <img src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`} />
+                    <div>{Math.floor(data?.main.temp)}°C</div>
+                </div><div className='small_info'>
+                    <div>
+                        <div>체감온도 {Math.floor(data?.main.feels_like)}°</div>
+                        <div>{Math.floor(data?.main.temp_max)}°/{Math.floor(data?.main.temp_min)}°</div>
+                    </div>
+                    <div>
+                        <div>습도 {data?.main.humidity}%</div>
+                        <div>{data?.weather[0].description}</div>
+                    </div>
+                </div>
+            </>
+        );
     }
     useEffect(() => {
         setArr(
@@ -36,23 +51,6 @@ export function Sky_div() {
             la = pos.coords.latitude
             lo = pos.coords.longitude
             getData();
-            setArr(
-                <>
-                    <div className='img_temp_container'>
-                        <img src={`http://openweathermap.org/img/wn/${da?.weather[0].icon}@2x.png`} />
-                        <div>{Math.floor(da?.main.temp)}°C</div>
-                    </div><div className='small_info'>
-                        <div>
-                            <div>체감온도 {Math.floor(da?.main.feels_like)}°</div>
-                            <div>{Math.floor(da?.main.temp_max)}°/{Math.floor(da?.main.temp_min)}°</div>
-                        </div>
-                        <div>
-                            <div>습도 {da?.main.humidity}%</div>
-                            <div>{da?.weather[0].description}</div>
-                        </div>
-                    </div>
-                </>
-            );
         })
     }, []);
 
