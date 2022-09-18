21b1cecf2e51de2c61f95305b421cc53f082fef1
jisu-d <lim16111611@gmail.com>
2022-09-18 23:05:22 +0900

    sever 코드 test

--- main/dist/server.js
+++ main/dist/server.js
@@ -150,36 +150,54 @@ const schoolScheduleDataParsing = (data) => {
     });
     const datas = [];
     for (let i of lastData) {
+        for (let j of datas) {
+            if (j.eventName !== i.eventName) {
+                datas.push({
+                    day: {
+                        start: i.day,
+                        last: i.day
+                    },
+                    eventName: i.eventName
+                });
+            }
+            else if (j.eventName === i.eventName) {
+                if (j.day.start > i.day) {
+                    j.day.start = i.day;
+                }
+                else if (j.day.last > i.day) {
+                    j.day.last = i.day;
+                }
+            }
+        }
         // console.log(datas);
-        if (datas) {
-            datas.push({
-                day: {
-                    start: i.day,
-                    last: i.day
-                },
-                eventName: i.eventName
-            });
-        }
-        else if (!datas) {
-            // console.log(2);
-            // for(let j of datas){
-            //     if(j.eventName !== i.eventName){
-            //             datas.push({
-            //                 day: {
-            //                     start: i.day,
-            //                     last: i.day
-            //                 },
-            //                 eventName: i.eventName
-            //             })
-            //         } else if(j.eventName === i.eventName){
-            //             if(j.day.start > i.day){
-            //                 j.day.start = i.day
-            //             } else if (j.day.last > i.day){
-            //                 j.day.last = i.day
-            //             }
-            //         }
-            // }
-        }
+        // if (datas) {
+        //     datas.push({
+        //         day: {
+        //             start: i.day,
+        //             last: i.day
+        //         },
+        //         eventName: i.eventName
+        //     })
+        // } else if (!datas) {
+        //     console.log(2);
+        //     for (let j of datas) {
+        //         if (j.eventName !== i.eventName) {
+        //             datas.push({
+        //                 day: {
+        //                     start: i.day,
+        //                     last: i.day
+        //                 },
+        //                 eventName: i.eventName
+        //             })
+        //         } else if (j.eventName === i.eventName) {
+        //             if (j.day.start > i.day) {
+        //                 j.day.start = i.day
+        //             } else if (j.day.last > i.day) {
+        //                 j.day.last = i.day
+        //             }
+        //         }
+        //     }
+        // }
         // i.eventName을 찾고 없으면 하나 넣음
         // start, end를 똑같이 씀
         // i.eventName가 있는 경우
