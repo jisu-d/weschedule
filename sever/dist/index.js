0476c76affe78254a52b8bc48d5d66ae0fc83c16
jisu-d <lim16111611@gmail.com>
2023-03-04 22:05:38 +0900

    cors all

--- sever/dist/index.js
+++ sever/dist/index.js
@@ -6,7 +6,7 @@ const fastify = Fastify({
     logger: true
 });
 fastify.register(fastifyCors, {
-    //origin:"https://weschedule.kr"
+    //origin:"https://weschedule.kr"dfghm
     origin: "/*"
 });
 fastify.register(route, { prefix: '/' }); //이거 슈밤바/f/main 해야함 /* 모든것
